531b25483bb845d1ebfee10ef5f595bc139deb79
Holovko Uaroslav <92970694+Lowckick@users.noreply.github.com>
2023-03-29 01:55:26 +0300

    Update DBWK.py

--- DBWK.py
+++ DBWK.py
@@ -1,59 +1,22 @@
 from pymongo import MongoClient
 from pandas import DataFrame
-from pymongo_get_database import get_database
 from flask import Flask, render_template, request
-from pymongo_get_database import CONNECTION_STRING
 from flask import request
 app = Flask(__name__)
-dbname = get_database()
+def get_database():
+ 
+   # Provide the mongodb atlas url to connect python to mongodb using pymongo
+   #"mongodb://azure-web-project:M73MVy790iHsITudPP4Q1xOeGjKLLeDueOKDM3h9hzZm0zmU8t1Ffc0uhTBFGQ74wz43RE8Shc6oACDbPizDQw%3D%3D@azure-web-project.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false&replicaSet=globaldb&maxIdleTimeMS=120000&appName=@azure-web-project@"
+   
+   CONNECTION_STRING = 'mongodb+srv://UaroslavH:BV9caZNzBmBPiNYQ@csgoskinexplorer.patitvp.mongodb.net/test'
+   client = MongoClient(CONNECTION_STRING)
+
+   return client['CS_SE']
+
+dbname=get_database()
 collection_name = dbname["user_1_items"]
-client = MongoClient(CONNECTION_STRING)
 
-item_1 = {
-  "Paint_seed" : "1000",
-  "item_name" : "AK-47 | Redline",
-  "Float" : 0.14075119793415  ,
-  "Condition" : "Minimal Wear",
-  "Market_price" : 2952.13 ,
-  "Float_cap" : "0.1-0.7"
-}
 
-item_2 = {
-  "Paint_seed" : "212",
-  "item_name" : "AWP | Asiimov",
-  "Float" : 0.21168099343777 ,
-  "Condition" : "Field-Tested",
-  "Market_price" : 5288.08 ,
-  "Float_cap" : "0.018-1"
-}
-item_3 = {
-  "Paint_seed" : "232",
-  "item_name" : "USP-S | Orion",
-  "Float" : 0.01807834580541 ,
-  "Condition" : "Factory New",
-  "Market_price" : 2125.12 ,
-  "Float_cap" : "0-0.5"
-}
-item_4 = {
-  "Paint_seed" : "120",
-  "item_name" : "Shadow Daggers | Ultraviolet",
-  "Float" : 0.27696374058723 ,
-  "Condition" : "Field-Tested",
-  "Market_price" : 3542.83 ,
-  "Float_cap" : "0.06-0.8"
-}
-item_5 = {
-  "Paint_seed" : "267",
-  "item_name" : "Glock-18 | Nuclear Garden",
-  "Float" : 0.08533399552107 ,
-  "Condition" : "Minimal Wear",
-  "Market_price" : 308.78 ,
-  "Float_cap" : "0-0.7"
-}
-collection_name.insert_many([item_1,item_2, item_3, item_4, item_5])
-#item_details = collection_name.find()
-#items_df = DataFrame(item_details)
-#print(items_df)
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
@@ -79,3 +42,4 @@ def index():
     return render_template('HTMLPage2_DB.html', data=data)
 if __name__ == '__main__':
     app.run()
+
